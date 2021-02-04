e25795ce89234264ab4a42f1fdfbf08761b96e43
WEremite <weremite@gmail.com>
2021-02-04 20:02:24 +0200

    Backup #1

--- Try/printf.php
+++ Try/printf.php
@@ -1,12 +1,23 @@
 <?php
+
+//Formatting a price with printf()
+$price  = 5;
+$tax    = 0.075;
+printf("The dish costs $%.2f", $price * (1 + $tax));
+//The dish costs $5.38
+
+
+//Zero-padding with printf()
 $zip    = '6520';
 $month  = 2;
 $day    = 6;
 $year   = 2007;
+printf("\nZIP is %05d and the date is %02d/%02d/%d", $zip, $month, $day, $year);
+//ZIP is 06520 and the date is 02/06/2007
 
-printf("ZIP is %05d and the date is %02d/%02d/%d", $zip, $month, $day, $year); //ZIP is 06520 and the date is
-                                                                                    // 02/06/2007
+//Displaying signs with printf()
+$min    = -40;
+$max    = 40;
+printf("\nThe computer can operate between %+d and %+d degrees Celcius.", $min, $max);
+//The computer can operate between -40 and +40 degrees Celsius.
 
-$price  = 5;
-$tax    = 0.075;
-printf("\nThe dish costs $%.2f", $price * (1 + $tax)); //The dish costs $5.38
